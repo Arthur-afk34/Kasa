83b25e963256ef4e5368f02843a5462538796c8c
Arthur-afk34 <seigneurarthur974@gmail.com>
2024-09-08 09:45:27 +0400

    5 typo

--- src/components/common/collapse/Collapse.js
+++ src/components/common/collapse/Collapse.js
@@ -3,7 +3,7 @@ import './Collapse.scss';
 import arrow from '../../../assets/images/prev.svg';
 
 /**
-La fonction Collapse est un composant qui représente une section de texte qui peut être réduite ou agrandie en cliquant sur un bouton (ici arrow).
+  .
 @param {*} props - les propriétés passées au composant, qui contiennent le titre et le contenu de la section.
 @returns - le composant Collapse qui affiche le titre et le contenu de la section, ainsi qu'un bouton type open/close pour réduire ou agrandir la section.
 */
@@ -14,11 +14,11 @@ function Collapse(props) {
   function toggle() {
     setIsOpen(!isOpen);
   }
-  
+
   // Définition de la classe CSS en fonction de l'état d'ouverture ou de fermeture du composant
   const arrowClassName = isOpen ? 'collapse__arrow arrow-open' : 'collapse__arrow arrow-closed';
   const collapsClassName = isOpen ? 'collapseText__box box-open' : 'collapseText__box box-closed';
-  
+
   return (
     <div className='collapse' >
       <button className={arrowClassName} onClick={toggle}>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,14 +15,58 @@ function Header() {
             <nav className='navigationLink'>
               <ul>
                 <li>
-                  <Link to="/kasa/">Accueil</Link>
+                  <Link to="/kasa/" className={(
+                    (nav) => (nav.isActive ? 
+                        "nav-active" : 
+                        "nav-unactive")
+                    )}>Accueil</Link>
                 </li>
                 <li>
-                  <Link to="/kasa/about">A propos</Link>
+                  <Link to="/kasa/about" className={(
+                    (nav) => (nav.isActive ? 
+                        "nav-active" : 
+                        "nav-unactive"))}>A propos</Link>
                 </li>
               </ul>
             </nav>
         </span>
+        // <div className='header'>
+        //     <Link to="/">
+        //         <div className='logo'>
+        //             <img 
+        //                 src="/images/Vector_K.svg"
+        //                 alt="Lettre K du logo Kasa" 
+        //                 className='logo__letterK' />
+        //             <img 
+        //                 src="/images/Group_A.svg" 
+        //                 alt="Lettre A du logo Kasa en forme de maison" 
+        //                 className='logo__letterAM' />
+        //             <img 
+        //                 src="/images/Vector_S.svg" 
+        //                 alt="Lettre S du logo Kasa" 
+        //                 className='logo__letterS' />
+        //             <img 
+        //                 src="/images/Vector_A.svg" 
+        //                 alt="Lettre A du logo Kasa" 
+        //                 className='logo__letterA' />
+        //         </div>
+        //     </Link>
+        //     <ul className='nav'>
+        //         <Link to="/" className={(
+        //             (nav) => (nav.isActive ? 
+        //                 "nav-active" : 
+        //                 "nav-unactive")
+        //             )}>
+        //             <li>Accueil</li>
+        //         </Link>
+        //         <Link to="/about" className={(
+        //             (nav) => (nav.isActive ? 
+        //                 "nav-active" : 
+        //                 "nav-unactive"))}>
+        //             <li>A Propos</li>
+        //         </Link>
+        //     </ul>
+        // </div>
   )
 }
   
